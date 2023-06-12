295a0e202889f73c6713f2e1d8878117f596c75d
chris530 <christopher.haessig@sentinelone.com>
2023-06-11 18:03:43 -0700

    added terraform

---
+++ terraform/eks.tf
@@ -0,0 +1,93 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "<18"
+  cluster_version = "1.26"
+  cluster_name    = var.cluster_name
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+  enable_irsa     = true
+  worker_groups = [
+    {
+      instance_type = "t3a.medium"
+      asg_max_size  = 1
+    }
+  ]
+}
+
+resource "helm_release" "karpenter" {
+  depends_on       = [module.eks.kubeconfig]
+  namespace        = "karpenter"
+  create_namespace = true
+  name             = "karpenter"
+  repository       = "https://charts.karpenter.sh"
+  chart            = "karpenter"
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_assumable_role_karpenter.iam_role_arn
+  }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+  set {
+    name  = "clusterEndpoint"
+    value = module.eks.cluster_endpoint
+  }
+}
+
+data "aws_iam_policy" "ssm_managed_instance" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_ssm_policy" {
+  role       = module.eks.worker_iam_role_name
+  policy_arn = data.aws_iam_policy.ssm_managed_instance.arn
+}
+
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "KarpenterNodeInstanceProfile-${var.cluster_name}"
+  role = module.eks.worker_iam_role_name
+}
+
+module "iam_assumable_role_karpenter" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "4.7.0"
+  create_role                   = true
+  role_name                     = "karpenter-controller-${var.cluster_name}"
+  provider_url                  = module.eks.cluster_oidc_issuer_url
+  oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
+}
+
+resource "aws_iam_role_policy" "karpenter_controller" {
+  name = "karpenter-policy-${var.cluster_name}"
+  role = module.iam_assumable_role_karpenter.iam_role_name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "kubernetes_manifest" "karp_config" {
+  yaml_body = file("karpenter.yaml")
+}
